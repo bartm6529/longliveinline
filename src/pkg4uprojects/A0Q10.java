7cf24996e4e6f5a0e8719e766d47e653843aa07b
bartm6529 <43249967+bartm6529@users.noreply.github.com>
2019-09-09 09:06:00 -0400

    Working on review.

--- src/pkg4uprojects/A0Q10.java
+++ src/pkg4uprojects/A0Q10.java
@@ -5,6 +5,8 @@
  */
 package pkg4uprojects;
 
+import java.util.Scanner;
+
 /**
  *
  * @author marley
@@ -15,7 +17,32 @@ public class A0Q10 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //input scanner
+        Scanner input = new Scanner(System.in);
+        
+        //tell user how to exit loop
+        System.out.println("You may type 'exit' at any time to stop the program.");
+        
+        //ask for word, find the length
+        System.out.println("Please enter a word.");
+        String word = input.nextLine();
+        int wordLength = word.length();
+        
+
+        //if less than two characters, get new word and length
+        while(wordLength < 2){
+            System.out.println("There are less than two characters. Please enter a new word.");
+            word = input.nextLine();
+            wordLength = word.length();  
+            
+        }
+        
+        //find first and last characters
+        char firstLetter = word.charAt(0);
+        char lastLetter = word.charAt(wordLength - 1);
+        
+        //tell them the first and last characters
+        System.out.println("The first letter is " + firstLetter + ". The last letter is " + lastLetter + ".");
     }
     
 }

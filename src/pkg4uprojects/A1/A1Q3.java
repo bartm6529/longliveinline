c007baa2c7147c23d67ad50b8fae74faeb1520e2
bartm6529 <bartm6529@HRH-IC0037416.wrdsb.ca>
2019-09-12 10:01:34 -0400

    Working on recursion assignment.

---
+++ src/pkg4uprojects/A1/A1Q3.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg4uprojects.A1;
+
+/**
+ *
+ * @author bartm6529
+ */
+public class A1Q3 {
+    //method to find sum of digits
+    public static int sumDigits(int n){
+        //if there is nothing left, return 0 (base case)
+        if(n % 10 == 0){
+            return 0;
+        //recursive call    
+        } else{
+            //take one digit off the int, add the digit just taken off
+            return sumDigits(n/10) + n % 10;
+        }
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //test code
+        int seventeen = sumDigits(17);
+        System.out.println(seventeen);
+        int oneTwoSix = sumDigits(126);
+        System.out.println(oneTwoSix);
+    }
+    
+}
---
+++ src/pkg4uprojects/A1/A1Q4.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg4uprojects.A1;
+
+/**
+ *
+ * @author bartm6529
+ */
+public class A1Q4 {
+
+    //method that counts sevens
+    public static int count7(int n){
+        //if there is nothing left, return 0 (base case)
+        if(n % 10 == 0){
+            return 0;
+        //recursive call
+        }else{
+            //take one digit off the int. If the int is 7, add it
+            
+        }
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
